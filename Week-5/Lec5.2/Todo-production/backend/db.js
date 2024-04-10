c9ed31174c4fd42a1e9e86b9cff996be95e391ab
Chitransh S <chitransh0210@gmail.com>
2024-04-10 20:13:26 +0530

    Testing the backend integration

--- Week-5/Lec5.2/Todo-production/backend/db.js
+++ Week-5/Lec5.2/Todo-production/backend/db.js
@@ -3,7 +3,6 @@ const { connection } = require('./connectionString.json')
 
 // connect to mongoDB
 mongoose.connect(connection)
-// mongoose.connect("mongodb+srv://garud:ZszWBBnpWw8ItAkE@cluster0.2cozjnc.mongodb.net/")
 
 const todoSchema = mongoose.Schema({
     title:String,
