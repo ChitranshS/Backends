d0c5fabcd180bde2373a35b7d07efcd38a9cb85d
Chitransh S <chitransh0210@gmail.com>
2024-04-28 04:48:57 +0530

    Adding TS with React Components

--- Week-9/Lec9.2/react-with-typescript/src/App.tsx
+++ Week-9/Lec9.2/react-with-typescript/src/App.tsx
@@ -12,11 +12,17 @@ interface TodoInterface {
   desc:string
   done:boolean
 }
+type TodoType = {
+  title:string
+  desc:string
+  done:boolean 
+
+}
 function Todo (props : TodoInterface)
 {
  return( 
  <div>
- <h1>{props.title}</h1>
+ <h1>{props.title}</h1> 
   <p>{props.desc}</p>
   </div>
  )
