aa3de3bd11a541747acffb4175c16810e396d600
Chitransh S <chitransh0210@gmail.com>
2024-04-28 04:28:32 +0530

    Adding TS with React Components

---
+++ Week-9/Lec9.2/react-with-typescript/src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+interface TodoInterface {
+  title:string
+  desc:string
+  done:boolean
+}
+function Todo (props : TodoInterface)
+{
+ return( 
+ <div>
+ <h1>{props.title}</h1>
+  <p>{props.desc}</p>
+  </div>
+ )
+}
+
+export default App
